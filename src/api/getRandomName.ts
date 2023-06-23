86e724724d416806ed9776293a61d69dc23f5df4
Oleg Tsyganov <redsmoke19@gmail.com>
2023-06-23 10:07:13 +0400

    fix: правит сервер на https

--- src/api/getRandomName.ts
+++ src/api/getRandomName.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const getRandomName = async () => {
-  const { data } = await axios<{ FirstName: string }>("http://api.randomdatatools.ru/?unescaped=false&params=FirstName");
+  const { data } = await axios<{ FirstName: string }>("https://api.randomdatatools.ru/?unescaped=false&params=FirstName");
   return data.FirstName;
 }
